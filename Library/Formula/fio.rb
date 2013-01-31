8200d0955aeda5b666e08b80452bac801887170e
David M. Lee <dlee@digium.com>
2013-01-31 09:29:12 -0600

    fio 2.0.13

--- Library/Formula/fio.rb
+++ Library/Formula/fio.rb
@@ -2,8 +2,13 @@ require 'formula'
 
 class Fio < Formula
   homepage 'http://freecode.com/projects/fio'
-  url 'http://brick.kernel.dk/snaps/fio-2.0.7.tar.bz2'
-  sha1 '597cca3a5843daeb3584ce52d090c58ca6632ef7'
+  url 'http://brick.kernel.dk/snaps/fio-2.0.13.tar.bz2'
+  sha1 'c2849065bc87bb3382a1b5877d9d0155b37cca17'
+
+  def patches
+    # Allow CFLAGS to be specified on the command line
+    DATA
+  end
 
   def install
     system "make", "prefix=#{prefix}",
@@ -13,3 +18,18 @@ class Fio < Formula
                    "install"
   end
 end
+
+__END__
+diff --git a/Makefile b/Makefile
+index ba8b997..38fdc33 100644
+--- a/Makefile
++++ b/Makefile
+@@ -130,7 +130,7 @@ FIO-VERSION-FILE: FORCE
+        @$(SHELL) ./FIO-VERSION-GEN
+ -include FIO-VERSION-FILE
+ 
+-CFLAGS += -DFIO_VERSION='"$(FIO_VERSION)"'
++override CFLAGS += -DFIO_VERSION='"$(FIO_VERSION)"'
+ 
+ .c.o: .depend FORCE
+        $(QUIET_CC)$(CC) -o $@ -c $(CFLAGS) $(CPPFLAGS) $<
